4ce8ebb1dace73bf7c701f435e4169d5aa9d0c12
Gleb Batykov <gleb.batykov@gmail.com>
2022-12-27 18:19:58 +0300

    Adds documentation

--- lib/src/controller_base.dart
+++ lib/src/controller_base.dart
@@ -1,7 +1,16 @@
 part of minerva_controller;
 
+/// Base class for all controller classes.
+///
+/// Controllers are used to configure the server.
 abstract class ControllerBase {
+  /// Initialization of the controller before starting work.
+  ///
+  /// This method is needed to initialize the resources necessary for operation, after the controller hits the server instance in which it will be used.
   FutureOr<void> initialize(ServerContext context) {}
 
+  /// The method is necessary to free up resources.
+  ///
+  /// You may need it if you decided to shut down the server yourself for some reason and used the dispose method of the Minerva class.
   FutureOr<void> dispose(ServerContext context) {}
 }
